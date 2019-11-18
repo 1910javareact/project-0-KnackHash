72bd8b4d73a6b7ff5bd1fbdaa70eb50de0afa79f
Joshua Roy <joshua.roy@revature.net>
2019-11-18 15:27:18 -0500

    added ReimbursementStatus

---
+++ src/models/reimbursementStatus.ts
@@ -0,0 +1,8 @@
+export class ReimbursementStatus {
+  statusId: number;
+  status: string;
+  constructor(statusId: number, status: string) {
+    this.statusId = statusId;
+    this.status = status;
+  }
+}
