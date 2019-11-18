a58c18e1766d8c94114bed98dddb5d1a61d369ae
Joshua Roy <joshua.roy@revature.net>
2019-11-18 14:46:54 -0500

    added user-router.ts

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,5 +1,6 @@
 import User from '../models/user'
 import { users } from "../database"
+import { getAllUsers, saveOneUser } from '../services/garden-service'
 
 function controllerGetUsers(req, res){
     let users = getAllUsers()
