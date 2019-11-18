42c22e51d1200e522cb8569e675376df7411c4c7
Joshua Roy <joshua.roy@revature.net>
2019-11-18 14:43:58 -0500

    added user.router.ts

---
+++ src/routers/user-router.ts
@@ -0,0 +1,33 @@
+import User from '../models/user'
+import { users } from "../database"
+
+function controllerGetUsers(req, res){
+    let users = getAllUsers()
+    if(users){       
+        res.json(users)
+    }else{
+        res.sendStatus(500)
+    }
+}
+
+userRouter.get('', controllerGetUsers)
+
+userRouter.post('', (req,res)=>{
+    let {body} = req
+    let newU = new User('',0,0)
+    for(let key in newU){
+        console.log(body[key]);
+        
+        if(body[key] === undefined){
+            res.status(400).send('Please include all user fields')
+            break;
+        }else{
+            newU[key] = body[key]
+        }
+    }
+    if(saveOneUser(newU)){
+        res.sendStatus(201)
+    }else {
+        res.sendStatus(500)
+    }
+})
