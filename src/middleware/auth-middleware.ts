5b3d794d019a3a71b0b6b142443cc7b2c0fc494f
Joshua Roy <joshua.roy@revature.net>
2019-11-19 18:07:28 -0500

    authorization

---
+++ src/middleware/auth-middleware.ts
@@ -0,0 +1,22 @@
+export function authorization(authRoles:string[]){
+
+return (req,res,next)=>{
+    let isAuth = false
+    //lets check for being logged in
+    if(!req.session.user){
+        res.status(401).send('Please Login')
+        return
+    }
+    for(let userRole of req.session.user.roles){
+        if(authRoles.includes(userRole)){
+            isAuth = true
+        }
+    }
+    if(isAuth){
+        next()
+    }else{
+        res.status(403).send('You are not authorized for this endpoint')
+    }
+}
+
+}
